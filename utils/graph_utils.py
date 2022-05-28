21e3c42c1ed77e94e0ef9f8d33d848b8b0bc0932
lbialik <liza.bialik@gmail.com>
2022-05-27 21:01:14 -0400

    axis label

--- utils/graph_utils.py
+++ utils/graph_utils.py
@@ -49,10 +49,12 @@ def plot_box_and_whisker(labels, data, title, save_location=None):
     fig, ax = plt.subplots()
     plt.title(title)
     ax.boxplot(data)
-    plt.xticks([1, 2, 3, 4], labels)
+    plt.xticks(np.arange(1, len(labels)+1), labels)
+    plt.xlabel("Measure")
+    plt.ylabel("Reading Rime (ms)")
+    if save_location:
+        plt.savefig(save_location)
     plt.show()
-    # if save_location:
-    #     plt.savefig(save_location)
 
 def print_box_and_whisker(segment_type):
     data = json.load(open("data/augmented_data.json"))
@@ -62,7 +64,7 @@ def print_box_and_whisker(segment_type):
         values = [data["SRC"][item][segment_type][measure]-data["ORC"][item][segment_type][measure] for item in data["ORC"]]
         graph_data.append(values if measure != "GPT2" else [val*20000000 for val in values])
     title = f"SRC - ORC ({segment_type})"
-    save_location = f"SRC-ORC({segment_type})_box_and_whisker"
+    save_location = f"plots/SRC-ORC({segment_type})_box_and_whisker"
     plot_box_and_whisker(measurements, graph_data, title, save_location)
 
 print_box_and_whisker("NP")
