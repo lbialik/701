171ae1be584814ade0c44d39b8ce8b79844d2638
lbialik <liza.bialik@gmail.com>
2022-06-01 02:14:55 -0400

    refactoring surprisal

--- utils/gpt_utils.py
+++ utils/gpt_utils.py
@@ -1,11 +1,8 @@
+from lib2to3.pgen2 import token
 import numpy as np
 import copy
 import torch
 from transformers import pipeline, AutoTokenizer, AutoModelForCausalLM
-
-# def score(model, tokens_tensor):
-#     loss=model(tokens_tensor, labels=tokens_tensor)[0]
-#     return np.exp(loss.cpu().detach().numpy())
 
 raw = True 
 
@@ -34,26 +31,43 @@ def print_top_x_predictions(next_token_logits, tokenizer):
         else:
             print(f'{tokenizer.decode(word_idx)} --> {val.item()/torch.sum(next_token_logits)}')
 
-def get_query_surprise(intro, query, model=None, dictionary=None):
-    # print(f'\ninput: {intro} [{query}]')
+def get_relative_clause_surprise(intro, relative_clause, model=None, dictionary=None):
+    # print(f'\ninput: {intro} [{relative_clause}]')
     tokenizer = AutoTokenizer.from_pretrained("gpt2")
     model = AutoModelForCausalLM.from_pretrained("gpt2")
     model.eval()
     tokenized_intro = tokenizer.encode(intro, return_tensors="pt")
-    tokenized_query = tokenizer.encode(' ' + query, return_tensors="pt")
-    query_next_token_logits = []
+    tokenized_relative_clause = tokenizer.encode(' ' + relative_clause, return_tensors="pt")
+    relative_clause_next_token_logits = []
     outputs = model(tokenized_intro)[0][:, -1, :][0]
-    for query_token in tokenized_query[0]:
+    for relative_clause_token in tokenized_relative_clause[0]:
         # print(f"\nSequence so far: {' '.join([tokenizer.decode(token) for token in tokenized_intro])}")
         outputs = model(tokenized_intro)
         next_token_logits = outputs[0][:, -1, :]
-        query_token_surprise = surprisal_of_word_norm(query_token, next_token_logits.detach())
-        # print(f'token surprise: {query_token_surprise}')
+        relative_clause_token_surprise = surprisal_of_word_norm(relative_clause_token, next_token_logits.detach())
+        # print(f'token surprise: {relative_clause_token_surprise}')
         # print_top_x_predictions(next_token_logits, tokenizer)
-        query_next_token_logits.append(next_token_logits.detach())
-        tokenized_intro = torch.tensor(np.array([np.append(tokenized_intro, query_token)]))
-    query_surprise = surprisal_of_words_norm(tokenized_query[0], query_next_token_logits)
-    # print('query suprise: ', query_surprise)
-    return query_surprise
+        relative_clause_next_token_logits.append(next_token_logits.detach())
+        tokenized_intro = torch.tensor(np.array([np.append(tokenized_intro, relative_clause_token)]))
+    relative_clause_surprise = surprisal_of_words_norm(tokenized_relative_clause[0], relative_clause_next_token_logits)
+    # print('relative_clause suprise: ', relative_clause_surprise)
+    return relative_clause_surprise
 
-# run_gpt("The mathematician that the chairman", "fired")
+def surprise(model, tokens_tensor, labels):
+    loss=model(tokens_tensor, labels=labels)[0]
+    return np.exp(loss.cpu().detach().numpy())
+
+intro = "The bus driver that"
+relative_clause = "drove the children home"
+sentence = intro + ' ' + relative_clause
+tokenizer = AutoTokenizer.from_pretrained("gpt2")
+model = AutoModelForCausalLM.from_pretrained("gpt2")
+model.eval()
+tokenized_intro = tokenizer.encode(intro, return_tensors="pt")
+tokenized_relative_clause = tokenizer.encode(' ' + relative_clause, return_tensors="pt")
+tokenized_sentence = tokenizer.encode(sentence, return_tensors="pt")
+masked_intro = torch.ones(len(tokenized_intro[0]), dtype=int) * -100
+masked_sentence = torch.cat((masked_intro, tokenized_relative_clause[0]))
+tokenized_labels = torch.reshape(masked_sentence, (1,-1))
+result = surprise(model, tokenized_sentence, tokenized_labels)
+print(result)
