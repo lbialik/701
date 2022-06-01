500dc7ab8b9447fe670de388106354530c73791a
lbialik <liza.bialik@gmail.com>
2022-06-01 16:45:03 -0400

    working on lstm loss/surprisal

--- utils/gpt_utils.py
+++ utils/gpt_utils.py
@@ -12,23 +12,19 @@ def print_top_x_predictions(next_token_logits, tokenizer):
     for word_idx, val in sorted(enumerate(next_token_logits[0]), key=lambda x:x[1], reverse=True)[:15]:
         print(f'{tokenizer.decode(word_idx)} --> {val.item()/torch.sum(next_token_logits)}')
 
-def get_relative_clause_surprise(intro, relative_clause, model=None, dictionary=None):
-    # print(f'\ninput: {intro} [{relative_clause}]')
+def get_query_surprise(intro, query, model=None, dictionary=None):
+    # print(f'\ninput: {intro} [{query}]')
     tokenizer = AutoTokenizer.from_pretrained("gpt2")
     model = AutoModelForCausalLM.from_pretrained("gpt2")
     model.eval()
-    sentence = intro + ' ' + relative_clause
+    sentence = intro + ' ' + query
     tokenizer = AutoTokenizer.from_pretrained("gpt2")
     model = AutoModelForCausalLM.from_pretrained("gpt2")
     model.eval()
     tokenized_intro = tokenizer.encode(intro, return_tensors="pt")
-    tokenized_relative_clause = tokenizer.encode(' ' + relative_clause, return_tensors="pt")
+    tokenized_query = tokenizer.encode(' ' + query, return_tensors="pt")
     tokenized_sentence = tokenizer.encode(sentence, return_tensors="pt")
     masked_intro = torch.ones(len(tokenized_intro[0]), dtype=int) * -100
-    masked_sentence = torch.cat((masked_intro, tokenized_relative_clause[0]))
+    masked_sentence = torch.cat((masked_intro, tokenized_query[0]))
     tokenized_labels = torch.reshape(masked_sentence, (1,-1))
     return surprise(model, tokenized_sentence, tokenized_labels)
-
-# intro = "The bus driver that"
-# relative_clause = "ignored the children"
-# print(get_relative_clause_surprise(intro, relative_clause))
--- utils/lstm_utils.py
+++ utils/lstm_utils.py
@@ -17,24 +17,15 @@ np.random.seed(50360)
 torch.backends.cudnn.deterministic = True
 torch.backends.cudnn.benchmark = False
 
-raw = True 
-
-def surprisal_of_words_norm(word_idxs, next_token_scores_list):
-    num = 0
-    denom = 0
-    assert(len(word_idxs) == len(next_token_scores_list))
-    for i, word_idx in enumerate(word_idxs):
-        num += next_token_scores_list[i][word_idx]
-        denom += torch.sum(next_token_scores_list[i])
-        # print(f'score = {next_token_scores_list[i][word_idx]}, scores sum = {torch.sum(next_token_scores_list[i])}')
-    if raw:
-        return float(1/(num/len(word_idxs)))
-    else:
-        return float(1/(denom-num))
+def surprise(model, lossfn, tokens_tensor, labels):
+    # model(input.view(-1, 1), model.init_hidden(1))
+    loss= lossfn(model(tokens_tensor), labels)
+    return np.exp(loss.cpu().detach().numpy())
 
 def get_query_surprise(intro, query, model, dictionary):
+    sentence = intro + ' ' + query
     intro, query = intro.split(), query.split()
-    for w in intro + query:
+    for w in sentence:
         if w not in dictionary.word2idx:
             print(w, ' not in vocab!')
 
@@ -44,39 +35,51 @@ def get_query_surprise(intro, query, model, dictionary):
     tokenized_query = [dictionary.word2idx[w]  if w in dictionary.word2idx
                                         else dictionary.word2idx["<unk>"]
                     for w in query]
-    query_next_token_scores = []
-    for query_token in tokenized_query:
-        # print(f'intro: {[dictionary.idx2word[w] for w in tokenized_intro]}')
-        # print(f'query: {dictionary.idx2word[query_token]}')
+    tokenized_sentence = [dictionary.word2idx[w]  if w in dictionary.word2idx
+                                        else dictionary.word2idx["<unk>"]
+                    for w in sentence]
 
-        input = torch.tensor(tokenized_intro, dtype=torch.long).cuda()
+    masked_intro = torch.ones(len(tokenized_intro), dtype=int) * -100
+    masked_sentence = torch.cat((masked_intro, tokenized_query))
+    tokenized_labels = torch.reshape(masked_sentence, (1,-1))
+    lossfn = torch.nn.MSELoss
+    query_surprise = surprise(model, lossfn, tokenized_sentence, tokenized_labels)
+    print(query_surprise)
 
-        ## Extract the hidden and output layers at each input token:
-        cur_sentence_output, cur_sentence_hidden = model(input.view(-1, 1), # (sequence_length, batch_size).
-                            model.init_hidden(1)) # one input at a time, thus batch_size = 1
-        next_word_scores = cur_sentence_output[-1].view(-1)
-        # print(next_word_scores)
-        if raw:
-            min_score, max_score = min(next_word_scores), max(next_word_scores) 
-            if min_score < 0:
-                # shift all scores to be positive
-                next_word_scores = next_word_scores - min_score
-        else:
-            next_word_scores = F.softmax(next_word_scores, dim=0)
-        query_next_token_scores.append(next_word_scores.detach())
-        tokenized_intro.append(query_token)
-    query_surprise = surprisal_of_words_norm(tokenized_query, query_next_token_scores)
-    return query_surprise
+    # query_next_token_scores = []
+    # for query_token in tokenized_query:
+    #     # print(f'intro: {[dictionary.idx2word[w] for w in tokenized_intro]}')
+    #     # print(f'query: {dictionary.idx2word[query_token]}')
+
+    #     input = torch.tensor(tokenized_intro, dtype=torch.long).cuda()
+
+    #     ## Extract the hidden and output layers at each input token:
+    #     cur_sentence_output, cur_sentence_hidden = model(input.view(-1, 1), # (sequence_length, batch_size).
+    #                         model.init_hidden(1)) # one input at a time, thus batch_size = 1
+    #     next_word_scores = cur_sentence_output[-1].view(-1)
+        
+    #     if raw:
+    #         min_score, max_score = min(next_word_scores), max(next_word_scores) 
+    #         if min_score < 0:
+    #             # shift all scores to be positive
+    #             next_word_scores = next_word_scores - min_score
+    #     else:
+    #         next_word_scores = F.softmax(next_word_scores, dim=0)
+    #     query_next_token_scores.append(next_word_scores.detach())
+    #     tokenized_intro.append(query_token)
+    # query_surprise = surprisal_of_words_norm(tokenized_query, query_next_token_scores)
+    # return query_surprise
 
 def set_up_model():
     torch.cuda.empty_cache()
     # Load Pre-trained Model
     fn = 'colorlessgreenRNNs/src/hidden650_batch128_dropout0.2_lr20.0.pt'
-    model_ = torch.load(fn)
     if torch.cuda.is_available():
         torch.device('cuda')
+        model_ = torch.load(fn)
     else:
         torch.device('cpu')
+        model_ = torch.load(fn, map_location=torch.device('cpu'))
     model = RNNModel("LSTM", 50001, 650, 650, 2, 0.2, False)
     model.load_state_dict(model_.state_dict())
     model = model.cuda()
